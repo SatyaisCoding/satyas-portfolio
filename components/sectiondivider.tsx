44c22a75fdd2057e67e08fac0802c912439f66ab
satya740 <satyaprakash74029@gmail.com>
2024-02-19 02:23:27 +0530

    sectiondivider

---
+++ components/sectiondivider.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { motion } from "framer-motion"
+
+
+
+export default function Sectiondivider() {
+  return (
+    <motion.div
+      className="bg-gray-500 my-14 h-16 w-1 rounded-full hidden sm:block "
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+    ></motion.div>
+  )
+}
